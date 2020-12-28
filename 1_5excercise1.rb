46f5d5c85a6799ddcd5e1886af0d561bc439c16e
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-28 15:58:02 -0500

    finished the question

---
+++ 1_5excercise1.rb
@@ -0,0 +1,10 @@
+# Make a new hash that contains a list of movies for each year. Each list of movies should be an array. Below is some data you can use.
+movies = {1999 => ["The Matrix", "Star Wars: Episode 1", "The Mummy"], 2009 => ["Avatar", "Star Trek, District 9"], 2019 => ["How to Train Your Dragon 3", "Toy Story 4", "Star Wars: Episode 9"]}
+p movies
+# Make a new array that contains each row of the buttons on a phone. Each row should be an array.
+# The rows on a phone are: 1 2 3, 4 5 6, 7 8 9, * 0 #
+phone_rows = [[1,2,3], [4,5,6], [7,8,9], ["*","0","#"]]
+p phone_rows
+# Make a new array that contains information about three countries. Each country should be a hash that has a name, a continent, and whether or not it is an island.
+countries = [{name: "Canada", continent: "America", island: "not island"}, {name: "USA", continent: "America", island: "not island"},{name: "Israel", continent: "Asia", island: "not island"}]
+p countries
