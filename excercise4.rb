d3fbc1d61bd3dd0228fa7c7c80e73e4c1aa821da
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-30 12:14:57 -0500

    need help understanding the question

---
+++ excercise4.rb
@@ -0,0 +1,24 @@
+# Copy the following array into a Ruby file:
+
+array =["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+# print "lowercase" if the string is all lowercase. print "long" if the string is more than 4 characters. print "long and lowercase" if the string's length is more than 4 characters and it's all lowercase. otherwise print the string itself The output should be:
+
+#input should be the array
+#loop through the array
+#if all letters are lowercase print "lowercase"
+#if more than 4 letters print "long"
+#if all "lowercase" and ore than 4 letters long print "long and lowercase"
+#otherwise print the actual string
+#end the loop
+
+
+
+# long
+# lowercase
+# lowercase
+# lowercase
+# long
+# lowercase
+# EX
+# EST
+# long and lowercase
