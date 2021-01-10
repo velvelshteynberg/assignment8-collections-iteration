6d4f5c1e85069ce6fc0832b37e3824955144a58b
Velvelshteynberg <Velvelshteynberg@outlook.com>
2021-01-10 12:51:46 -0500

    question completed

--- excercise8.rb
+++ excercise8.rb
@@ -10,15 +10,10 @@
 # Row 3 seat 4 is free.
 # Hint: Ruby has a method called each_with_index that you can use in place of each, which may come in handy in this problem.
 
-# For each available seat, use gets.chomp to prompt your user to choose whether they want to claim that spot. If they indicate they want to claim a seat, prompt them for their name and insert it into the array to show that they've been seated, like so:
+# For each available seat, use gets.chomp to prompt your user to choose whether they want to claim that spot. 
+#If they indicate they want to claim a seat, prompt them for their name and insert it into the array to show that they've been seated, like so:
 
-# Row 1 seat 1 is free. Do you want to sit there? (y/n) # user says 'n'
-# Row 1 seat 3 is free. Do you want to sit there? (y/n) # user says 'n'
-# Row 1 seat 4 is free. Do you want to sit there? (y/n) # user says 'n'
-# Row 2 seat 2 is free. Do you want to sit there? (y/n) # user says 'n'
-# Row 3 seat 3 is free. Do you want to sit there? (y/n) # user says 'n'
-# Row 3 seat 4 is free. Do you want to sit there? (y/n) # user says 'y'
-# What is your name? # user says "Tama"
+
 # The data should now look like this:
 
 # [
@@ -35,17 +30,30 @@ seating_arrangement = [[nil, "Pumpkin", nil, nil],["Socks", nil, "Mimi", nil],["
 # Display the list of available seats to your user, like so:# Row 1 seat 1 is free.# Row 1 seat 3 is free.# Row 1 seat 4 is free.# Row 2 seat 2 is free.# Row 3 seat 3 is free.# Row 3 seat 4 is free.
 #pseudo code
 #loop through the array 
-seating_arrangement.each do |individual_row|
+puts "What is your name?"
+user_name = gets.chomp
+seating_arrangement.each_with_index do |individual_row, index1|
 #loop through each row's array
-individual_row.each do |individual_seating_position|
+individual_row.each_with_index do |individual_seating_position, index|
 #find all the places that nil is written with a flow control 
 if individual_seating_position == nil
-    #if nil is written you should display "(index position + 1(within the row array)) seat in the "(index position + 1 (within the seating arrangement array) row is empty and avialbale for you to sit in"
-    puts "seat #{(((individual_row.index("individual_seating_position").to_i)+1)} is available in row #{(((seating_arrangement.index("individual_row").to_i))+1)}"
-    #end the flow control
+    puts "seat #{index + 1} in row #{index1 + 1} is currently available "
+    puts "Would you like to sit here? (yes/no)"
+    answer = gets.chomp
+    if answer == "yes"
+        # individual_seating_position[index] = user_name
+    puts "you are now sitting in seat #{index + 1} in row #{index + 1}"
+    return false;
+    elsif answer == "no"
+        puts "okay. lets move on to the next seat"
+    end 
+#end the flow control
 end
 #end the individual seating position loop 
 end
 #end the seating arrangment loop
 end
+
+
+
 
